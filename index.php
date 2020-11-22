1d0393c314a068add7f30d018331e732f7b7b73c
Mohamed-abidat <Mohamed-abidat@users.noreply.github.com>
2020-11-22 12:30:55 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Chat</title>
+</head>
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<body>
+
+	<div id="wrapper">
+		
+		<div id="left_pannel">
+			
+		</div>
+		<div id="right_pannel">
+			
+			<div id="header">My Chat</div>
+			
+			<div id="container" style="display: flex;">
+				
+				<div id="inner_left_pannel">
+					
+				</div>
+
+				<div id="inner_right_pannel">
+					
+				</div>
+			</div>
+		
+		</div>
+	</div>
+
+</body>
+</html>
